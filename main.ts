6f80873131900349e4bb0928403730b89fdd74df
Kyriacos Louca <kyrlouca@gmail.com>
2021-02-13 15:49:56 +0200

    checked

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 import * as path from 'https://deno.land/std@0.86.0/path/mod.ts';
 import { Path, WINDOWS_SEPS } from 'https://deno.land/x/path/mod.ts';
-import { exists as fexists } from 'https://deno.land/std@0.86.0/fs/mod.ts';
+import { delay } from 'https://deno.land/x/delay@v0.2.0/mod.ts';
+import {
+  exists as fexists,
+  existsSync,
+  existsSync as sexists,
+} from 'https://deno.land/std@0.86.0/fs/mod.ts';
 import getFiles, {
   exists,
   fileExt,
@@ -39,9 +44,7 @@ async function justCopy(source: string, destination: string): Promise<void> {
   nf2.close;
 }
 
-console.log('http://localhost:8080/');
-console.log('hello');
-console.log('abc');
+console.log('PensionDenoApi started=> http://localhost:8080/');
 
 app.use((next) => async (c) => {
   c.set('Name', 'Mu Shan');
@@ -55,10 +58,12 @@ app.use((next) => async (c) => {
 });
 
 const validateDocument: HandlerFunc = async (c) => {
+  //http://localhost:8000/validate?documentId=3838&fundId=7
+
   const { fundId = 0, documentId = 0 } = c.queryParams;
   if (fundId == 0 || documentId == 0) {
     console.log('Validation Arguments: fundId, documentId');
-    return;
+    return `Failed Validation: Arguments: fundId, documentId`;
   }
   const p = await Deno.run({
     cmd: [
@@ -70,13 +75,14 @@ const validateDocument: HandlerFunc = async (c) => {
     ],
   });
   p.close;
-  return documentId;
+  return `Validated document: ${documentId}`;
 };
 const deleteDocument: HandlerFunc = async (c) => {
+  //http://localhost:8000/delete?documentId=3811
   const { documentId = 0 } = c.queryParams;
   if (documentId == 0) {
     console.log('DeleteDocumentData Arguments: documentId');
-    return;
+    return 'Failed deleteDocument. Arguments: documentId';
   }
   const p = await Deno.run({
     cmd: [
@@ -86,15 +92,16 @@ const deleteDocument: HandlerFunc = async (c) => {
       `${documentId}`,
     ],
   });
-  return `${documentId}`;
+  return `Deleted DocumentId:${documentId}`;
 };
 
 const aggregate: HandlerFunc = async (c) => {
-  // Arguments: UserId, ModuleCode, year, quarter)
+  // Arguments: UserId, ModuleCode, year, quarter
+  //http://localhost:8000/aggregate?userId=4&moduleCode=qra&year=2020&quarter=3
   const { userId = 0, moduleCode = '', year = 0, quarter = 0 } = c.queryParams;
   if (userId == 0 || moduleCode == '' || year == 0) {
-    console.log('aggregate Arguments: UserId, ModuleCode, year, quarter)');
-    return;
+    console.log('aggregate Arguments: UserId, ModuleCode, year, quarter');
+    return `Failed: aggregate Arguments: UserId, ModuleCode, year, quarter`;
   }
   const p = await Deno.run({
     cmd: [
@@ -107,11 +114,13 @@ const aggregate: HandlerFunc = async (c) => {
       `${quarter}`,
     ],
   });
-  return `${moduleCode}`;
+  return `Finished aggregation for module: ${moduleCode} year:${year} quarter ${quarter}`;
 };
 
 const downloadXbrl: HandlerFunc = async (ctx: Context) => {
+  //http://localhost:8000/downloadXbrl?documentId=3838&filename=C:\Users\kyrlo\soft\DbTest\public\babamaba7.xbrl
   //pass the document id and the filepath
+  //the program will first create the xbrl in the filename provided and then pass it as an attachement
   //deno requires that the filePath must be under /public
   // the name of the file  should be unique but also have some meaning because
   // it will be used to zip and send to eipa
@@ -119,8 +128,8 @@ const downloadXbrl: HandlerFunc = async (ctx: Context) => {
 
   const { documentId = 0, filename = '' } = ctx.queryParams;
   if (!documentId) {
-    console.log('downloadXbrlFile Arguments (api): documentId');
-    return;
+    console.log('downloadXbrlFile Arguments (api): documentId fullfilepath');
+    return `Failed downloadXbrl: Arguments :documentId`;
   }
   if (!filename) {
     //user did not specified filename. It's ok.
@@ -136,7 +145,6 @@ const downloadXbrl: HandlerFunc = async (ctx: Context) => {
     return;
   }
 
-
   if (filename) {
     try {
       const dir = path.dirname(filename);
@@ -147,10 +155,10 @@ const downloadXbrl: HandlerFunc = async (ctx: Context) => {
       }
     } catch (e) {
       console.log(e);
+      return `failed to find Folder for ${filename}`;
     }
   }
 
-  
   const p = await Deno.run({
     cmd: [
       'cmd',
@@ -161,52 +169,88 @@ const downloadXbrl: HandlerFunc = async (ctx: Context) => {
     ],
   });
 
-  const fileCreated = await fexists(filename);
+  //give it some time to create the file
+
+  // setTimeout(
+  //   async (ctx, filename) => {
+  //     const fileCreated = sexists(filename);
+  //     if (!fileCreated) {
+  //       console.log(`vvvvXbrl File was not created. file:${filename}`);
+  //       return `vvvvXbrl File was not created. file:${filename}`;
+  //     } else {
+  //       console.log(`yyycreated. file:${filename}`);
+  //       const fileBuf =  Deno.readFileSync(filename);
+  //       if (!fileBuf) {
+  //         console.log(`fff Xbrl File was not created. file:${filename}`);
+  //         return `fff Xbrl File was not created. file:${filename}`;
+  //       }
+  //       const ff=path.basename(filename);
+  //       ctx.response.headers.set('Content-Type', 'application/octet-stream');
+  //       ctx.response.headers.set(
+  //         'Content-Disposition',
+  //         `attachment;filename=${ff}`
+  //       );
+  //       return fileBuf;
+  //     }
+  //   },
+  //   4000,
+  //   ctx,
+  //   filename
+  // );
+
+
+  await delay(6000);
+
+  const fileCreated = existsSync(filename);
   if (!fileCreated) {
-    console.log(`File was not created. file:${filename}`);
-    return;
+    console.log(`Xbrl File was not created. file:${filename}`);
+    return `Xbrl File was not created. file:${filename}`;
   }
-
-  const fileBuf = await Deno.readFile(filename);
+  
+  
+  const fileBuf = Deno.readFileSync(filename);
   if (!fileBuf) {
-    console.log(`File was not created. file:${filename}`);
-    return;
+    console.log(`Xbrl File was not created. file:${filename}`);
+    return `Xbrl File was not created. file:${filename}`;
   }
-
+  const ff = path.basename(filename);
   ctx.response.headers.set('Content-Type', 'application/octet-stream');
-  ctx.response.headers.set(
-    'Content-Disposition',
-    `attachment;filename=${filename}`
-  );
+  ctx.response.headers.set('Content-Disposition', `attachment;filename=${ff}`);
   return fileBuf;
 };
-
 
 const uploadExcel: HandlerFunc = async (ctx: Context) => {
   //pass  filePath, fundId, userId, module, year, quarter
   //http://localhost:8000/uploadExcel?filename=C:\Users\kyrlo\OneDrive\Documents\PENSION%20DSIS\My%20Testing\UserExcel\AXI_Anual_Exempted_L6.xlsx&fundId=9&userId=19&module=axi&year=2020&quarter=2
 
-  const {  filename = '',fundId=0,userId=0,module='',year=0,quarter=-1 } = ctx.queryParams;
-  
-  if (!filename || !fundId || !userId || !module || !year || quarter==-1 ){
-    console.log('uploadExcel arguments: filePath, fundId, userId, module, year, quarter')
-    return;
-  }
-  
- 
-    try {
-      
-      const isFileExists = await fexists(filename);
-      if (!isFileExists) {
-        console.log(`File does NOT exist`)
-        console.log(`file :${filename} `);
-        return;
-      }
-    } catch (e) {
-      console.log(e);
-    }  
+  const {
+    filename = '',
+    fundId = 0,
+    userId = 0,
+    module = '',
+    year = 0,
+    quarter = -1,
+  } = ctx.queryParams;
 
-  
+  if (!filename || !fundId || !userId || !module || !year || quarter == -1) {
+    console.log(
+      'uploadExcel arguments: filePath, fundId, userId, module, year, quarter'
+    );
+    return   'uploadExcel arguments: filePath, fundId, userId, module, year, quarter';
+  }
+
+  try {
+    const isFileExists = await fexists(filename);
+    if (!isFileExists) {
+      console.log(`File does NOT exist`);
+      console.log(`file :${filename} `);
+      return `Does NOT exist. File :${filename}`;
+    }
+  } catch (e) {
+    console.log(e);
+    return `{e}`
+  }
+
   const p = await Deno.run({
     cmd: [
       'cmd',
@@ -220,58 +264,13 @@ const uploadExcel: HandlerFunc = async (ctx: Context) => {
       `${quarter}`,
     ],
   });
-
-  
-  
-};
-
-
-const copyfile = async (sourceName: string, destName: string) => {
-  const __dirname = new URL('.', import.meta.url).pathname;
-
-  // sourceName = `DSIS_0007_qri_2020_1_2021-02-05-200025.xbrl`;
-  var sourcePath = path.join(filesData.excelFolder, sourceName);
-
-  // destName = `abcd.xbrl`;
-  const root = __dirname.slice(1);
-  const destPath = path.join(root, 'public', destName);
-
-  try {
-    const sourceFile = await Deno.open(sourcePath);
-    const decoder = new TextDecoder('utf-8');
-    const textF = decoder.decode(await Deno.readAll(sourceFile));
-
-    const destFile = await Deno.open(destPath, { write: true, create: true });
-    await Deno.write(destFile.rid, new TextEncoder().encode(textF));
-    destFile.close;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const fetchFile: HandlerFunc = async (ctx: Context) => {
-  //download a file  from local storage to the browser
-  //first we need to copy the file in pulic which is under the root
-  var s2 = path.join(
-    filesData.excelFolder,
-    `DSIS_0007_qri_2020_1_2021-02-05-200025.xbrl`
-  );
-  await copyfile('ab', 'cd');
-
-  const fileBuf = await Deno.readFile('./public/DSIS_ari.xbrl');
-  ctx.response.headers.set('Content-Type', 'application/octet-stream');
-  ctx.response.headers.set(
-    'Content-Disposition',
-    'attachment;filename=somefile.xbrl'
-  );
-  return fileBuf;
+  return `Uploaded file :${filename}`
 };
 
 app
   .get('/', () => {
-    return 'root';
+    return 'in root doing nothing';
   })
-  .get('/test', fetchFile)
   .get('/delete', deleteDocument)
   .get('/validate', validateDocument)
   .get('/aggregate', aggregate)
